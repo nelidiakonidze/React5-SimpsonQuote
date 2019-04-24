96f5b95722659cf9cc95ea9cdaeec5cb2b5fc02e
Neli <diakonidzeneli@gmail.com>
2019-04-24 14:49:22 +0200

    just trying new react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import GenerateEmployee from './GenerateEmployee';
+import DisplayEmployee from './DisplayEmployee';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GenerateEmployee />
+      <DisplayEmployee employee={sampleEmployee} />
     </div>
   );
 }
 
+
+const sampleEmployee = {
+  gender: 'male',
+  name: {
+    title: 'mr',
+    first: 'mathys',
+    last: 'aubert'
+  },
+  location: {
+    street: '9467 rue gasparin',
+    city: 'perpignan',
+    postcode: '90208'
+  },
+  email: 'mathys.aubert@example.com',
+  picture: {
+    medium: 'https://randomuser.me/api/portraits/med/men/66.jpg'
+  }
+};
+
+
 export default App;
+
