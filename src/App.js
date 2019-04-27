b3e33abbc8b9129c8161032c67cb68719c6ea4b8
Neli <diakonidzeneli@gmail.com>
2019-04-27 14:44:55 +0200

    created a Displaay Quote component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
+import DisplayQuote from './DisplayQuote.js';
 
 function App() {
   return (
     <div className="App">
       <h1>Click the button to get a Simpson experince!</h1>
+      <DisplayQuote />
     </div>
   );
 }
 
 export default App;
-
