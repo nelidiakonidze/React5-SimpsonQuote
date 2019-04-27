f48998dc9b67c0b0784f561cb8d59c7195fc1003
Neli <diakonidzeneli@gmail.com>
2019-04-27 18:04:44 +0200

    final things

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import DisplayQuote from './DisplayQuote.js';
 function App() {
   return (
     <div className="App">
-      <h1 className="header">Simpson's experince!</h1>
+      <h1 className="header">Simpson's experience!</h1>
       <DisplayQuote />
     </div>
   );
