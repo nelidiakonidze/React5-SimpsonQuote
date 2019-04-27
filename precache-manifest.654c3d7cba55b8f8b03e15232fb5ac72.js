3aad6a0abfcf4212f9a8410e1bf2eeb86c3d5087
Neli <diakonidzeneli@gmail.com>
2019-04-27 16:38:40 +0200

    Updates

---
+++ precache-manifest.654c3d7cba55b8f8b03e15232fb5ac72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b924ba2acc4cc617dd9794a2bc6a3be",
+    "url": "/React5-SimpsonQuote/index.html"
+  },
+  {
+    "revision": "465c80cbe6b098eefb64",
+    "url": "/React5-SimpsonQuote/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "0930f3ac25704a0c1e19",
+    "url": "/React5-SimpsonQuote/static/css/main.039a5eeb.chunk.css"
+  },
+  {
+    "revision": "465c80cbe6b098eefb64",
+    "url": "/React5-SimpsonQuote/static/js/2.f1b68328.chunk.js"
+  },
+  {
+    "revision": "0930f3ac25704a0c1e19",
+    "url": "/React5-SimpsonQuote/static/js/main.93c0bc7f.chunk.js"
+  },
+  {
+    "revision": "77ffe5eb596373387ec0",
+    "url": "/React5-SimpsonQuote/static/js/runtime~main.b33540c7.js"
+  }
+]);
